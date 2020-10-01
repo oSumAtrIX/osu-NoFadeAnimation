9d08e85d6e964f57e1914850964deb21469a7a89
oSumAtrIX <johan.melkonyan1@web.de>
2020-10-01 07:27:48 +0200

    Beautify code

--- osu!NoFadeAnimation/Program.cs
+++ osu!NoFadeAnimation/Program.cs
@@ -11,40 +11,40 @@ using static System.IO.Path;
 using static System.IO.File;
 using static System.Console;
 
+namespace osu_NoFadeAnimation {
+  class Program {
+    static void Main(string[] args) {
+      if (args.Length != 1) {
+        WriteLine($ "Drag & drop osu!.exe onto {GetExecutingAssembly().GetName().Name}.exe");
+        ReadLine();
+        return;
+      }
 
+      string path = args[0];
+      var module = ModuleDefMD.Load(path);
 
-namespace osu_NoFadeAnimation
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length != 0x1)
-            {
-                WriteLine($"Drag & drop osu!.exe onto {GetExecutingAssembly().GetName().Name}.exe"); ReadLine();
-                return;
-            }
+      //RIDs will change for builds other than build 20200918.3.
+      IList < Instruction > entryMethodInstr = module.ResolveMethod(8910).Body.Instructions,
+      hitArmMethodInstr = module.ResolveMethod(2729).Body.Instructions,
+      hashCheckMethodInstr = module.ResolveMethod(8719).Body.Instructions;
 
-            string path = args[0x0];
-            var module = ModuleDefMD.Load(path);
+      entryMethodInstr[7].OpCode = Brfalse_S;
 
-            //RIDs will change for builds other than build 20200918.3.
-            IList<Instruction>
-                entryMethodInstr = module.ResolveMethod(8910).Body.Instructions,
-                hitArmMethodInstr = module.ResolveMethod(2729).Body.Instructions,
-                hashCheckMethodInstr = module.ResolveMethod(8719).Body.Instructions;
+      //Offsets might change for builds other than build 20200918.3.
+      var offsets = new[] {
+        18,
+        39,
+        61
+      };
+      for (int i = 0; i < offsets.Length; i++) hitArmMethodInstr[offsets[i]].OpCode = Nop;
 
-            entryMethodInstr[7].OpCode = Brfalse_S;
-            
-            //Offsets might change for builds other than build 20200918.3.
-            var offsets = new[] { 18, 39, 61 };
-            for (int i = 0; i < offsets.Length; i++) hitArmMethodInstr[offsets[i]].OpCode = Nop;
+      hashCheckMethodInstr[376].OpCode = Nop;
+      hashCheckMethodInstr[377] = Ldstr.ToInstruction(string.Join("", Create().ComputeHash(ReadAllBytes(path)).Select(s =>s.ToString("x2"))));
 
-            hashCheckMethodInstr[376].OpCode = Nop;
-            hashCheckMethodInstr[377] = Ldstr.ToInstruction(string.Join("", Create().ComputeHash(ReadAllBytes(path)).Select(s => s.ToString("x2"))));
+      module.Write(Combine(GetDirectoryName(path), $ "{GetFileNameWithoutExtension(path)}-patched{GetExtension(path)}"), new ModuleWriterOptions(module) {
+        Logger = new DummyLogger(null)
+      });
 
-            module.Write(Combine(GetDirectoryName(path), $"{GetFileNameWithoutExtension(path)}-patched{GetExtension(path)}"), new ModuleWriterOptions(module) { Logger = new DummyLogger(null)});
-
-        }
     }
+  }
 }
